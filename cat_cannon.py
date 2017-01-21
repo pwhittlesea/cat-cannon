310169b77727f9b484f6aa43da1a472c4f742ae3
Phillip Whittlesea <pw.github@thega.me.uk>
2017-01-21 14:39:45 +0000

    Capture a simple image

--- cat_cannon.py
+++ cat_cannon.py
@@ -1,1 +1,18 @@
 # -*- coding: utf-8 -*-
+
+from time import sleep
+from picamera import PiCamera
+
+# Explicitly open a new file called my_image.jpg
+my_file = open('my_image.jpg', 'wb')
+
+# Create the camera object
+camera = PiCamera()
+camera.start_preview()
+
+# Camera warm-up time
+sleep(2)
+
+# Save the image to a file
+camera.capture(my_file)
+my_file.close()
